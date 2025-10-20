6b9471a947582afecf3ebda0b7ab2959232d6ce2
MariaIsaabell <pmariaisabel229@gmail.com>
2025-10-20 11:03:36 -0300

    UPDATE script do Botão Primeira Letra Maíuscula

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 /* Programa - Modelo 1 */
 
-window.document.addEventListener("DOMContentLoaded", function(){
+window.document.addEventListener("DOMContentLoaded", function () {
+  /* Botão Letra Maiúscula */
+  window.document
+    .getElementById("btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toUpperCase();
+    });
+  /* Botão Letra Maiúscula */
+  window.document
+    .getElementById("btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toLowerCase();
+    });
 
-    /* Botão Letra Maiúscula */
-    window.document.getElementById("btnMaiuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value 
-        document.querySelector("#result").innerHTML = inputText.toUpperCase()
-    })
+  window.document
+    .getElementById("btnPrimeiraLetraMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      let resultado =
+        inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
+      document.querySelector("#result").innerHTML = resultado;
+    });
 
-     window.document.getElementById("btnMinuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value 
-        document.querySelector("#result").innerHTML = inputText.toLowerCase()
-    })
-
-})
+  window.document
+    .getElementById("btnLimpar")
+    .addEventListener("click", function () {
+      document.querySelector("#input-text").value = " ";
+      document.querySelector("#result").innerHTML = " ";
+    });
+});
