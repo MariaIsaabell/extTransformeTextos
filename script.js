beea56dd8b67531d6e07f8e32c8f496d5b135f0d
MariaIsaabell <pmariaisabel229@gmail.com>
2025-10-20 11:32:38 -0300

    UPDATE Criação do Botão Extra - Negrito

--- script.js
+++ script.js
@@ -20,8 +20,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
     .getElementById("btnPrimeiraLetraMaiuscula")
     .addEventListener("click", function () {
       let inputText = document.querySelector("#input-text").value;
-      let resultado =
-        inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
+      let resultado = inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
       document.querySelector("#result").innerHTML = resultado;
     });
 
@@ -30,5 +29,13 @@ window.document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function () {
       document.querySelector("#input-text").value = " ";
       document.querySelector("#result").innerHTML = " ";
+    });
+
+  window.document
+    .getElementById("btnNegrito")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      let resultado = `<strong>${inputText}</strong>`;
+      document.querySelector("#result").innerHTML = resultado;
     });
 });
